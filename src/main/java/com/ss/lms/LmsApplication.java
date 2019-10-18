11a6b5ffebbbfd7963cea7d95c4e1bf6cd2547f1
NicolaeVSS <nicolae.vasile@smoothstack.com>
2019-10-17 21:49:23 -0400

    admin is now discoverable via @EnableEurekaClient

--- src/main/java/com/ss/lms/LmsApplication.java
+++ src/main/java/com/ss/lms/LmsApplication.java
@@ -1,13 +1,15 @@
 package com.ss.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LmsApplication 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello my World!");
+		System.out.println("ADMIN");
 		SpringApplication.run(LmsApplication.class, args);
 	}
 }
